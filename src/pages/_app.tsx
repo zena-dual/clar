33680309f5d44a2f7fb6d47722f19859a97f24f7
zena <daikidigdag@gmail.com>
2021-01-03 20:58:54 +0900

    chore: Head / AppBar の記述箇所を _app に移動

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import React, { useEffect } from 'react';
+import Head from 'next/head';
+import { makeStyles } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 import 'destyle.css';
 import { theme } from '../app/theme';
+import { AppBar } from '../components/AppBar';
+
+const useStyles = makeStyles(() => ({
+  body: {
+    marginTop: 60,
+    minHeight: 'calc(100vh - 60px)',
+  },
+}));
 
 interface Props<ComponentProps> {
   Component: (props: ComponentProps) => JSX.Element;
@@ -9,6 +19,8 @@ interface Props<ComponentProps> {
 }
 
 const App = ({ Component, pageProps }: Props<any>) => {
+  const classes = useStyles();
+
   useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
@@ -18,7 +30,16 @@ const App = ({ Component, pageProps }: Props<any>) => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+      </Head>
+
+      <AppBar />
+
+      <div className={classes.body}>
+        <Component {...pageProps} />
+      </div>
     </ThemeProvider>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,13 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
 import { APP_NAME } from '../app/settings';
-import { AppBar } from '../components/AppBar';
 
 const Index: NextPage = () => {
   return (
     <>
       <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-
         <title>{`トップ | ${APP_NAME}`}</title>
       </Head>
-
-      <AppBar />
 
       <div>initial page</div>
     </>
